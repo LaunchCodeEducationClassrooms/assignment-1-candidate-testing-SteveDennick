fba4b9a4e34512f13d970491e800980fbad2837f
Stephen Dennick <stdennick@gmail.com>
2021-02-16 05:57:43 +0000

    Some formatting

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,9 +6,9 @@ let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let questions = ["Who was the first American woman in space? ", "True or false: 5000 meters = 5 kilometers. ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"]
-let candidateAnswers = []
-let grade = ""
-let questionInputs = ""
+let candidateAnswers = [];
+let grade = "";
+let questionInputs = "";
 
 function askForName() {
   return candidateName = input.question("Enter your name here: ");
@@ -19,18 +19,21 @@ function askQuestion() {
     questionsInput = input.question(`${[i + 1]}) ${questions[i]}`);
     candidateAnswers.push(questionsInput);
     console.log(`Your Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}`)
-    console.log("")    
+    console.log("");   
   }
 }
+
 let noOfCorrectAnswers = 0
 function tallyQuiz() {
   let n = 0;
   while (n < correctAnswers.length) {
     if (candidateAnswers[n].toLowerCase() === correctAnswers[n].toLowerCase()) {
-      noOfCorrectAnswers += 1;}
-      n++;
-      }
+      noOfCorrectAnswers += 1;
     }
+    n++;
+  }
+}
+
 let status = "";
 function gradeQuiz(){
   tallyQuiz();
